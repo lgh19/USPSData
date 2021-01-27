356044cb1b684349f60993a06907277d47d35873
lgh19 <67384606+lgh19@users.noreply.github.com>
2021-01-27 15:59:05 -0500

    Update usps_data.py

--- usps_data.py
+++ usps_data.py
@@ -1,23 +1,41 @@
+#example of how to run the code:
+        
+#create an object of the class (a) and pass in 2 csv files, the first being a quarterly file and the second being the file with all quarters aggregated in one file
+a = USPSData('USPS_09_2014.csv', 'USPSVacancyData.csv)
+#^this will run the entire code and the output will be a new quarterly csv file with only the Pennsylvania tracts and the new columns added, 
+#and a second csv file that has all the previous quarterly data and this new quarter's data appended to it 
+   
+
+
 import pandas as pd
 import numpy as np
 import math
 
 class USPSData:
     fileName = ""
+    allPghFileName = ""
     global df
+    global pghdf
     global endFileName
     global file
-    def __init__(self, fileName):
+    global finalPghFile
+    global quarter
+    
+    def __init__(self, fileName, allPghFileName):
         self.endFileName = str(fileName)
         a = self.endFileName.split('.')
         self.endFileName = a[0]
         self.file = str(fileName)
         s = self.file.split('_')
-        quarter = s[2]
+        self.quarter = s[2]
         self.df = pd.DataFrame(data=None, columns=None)
         self.fileName = fileName
         self.df = pd.read_csv(fileName)
-        print(self.df)
+        self.df.columns= self.df.columns.str.upper()
+        #print(self.df)
+        self.pghdf = pd.read_csv(allPghFileName)
+        self.finalPghFile = str(allPghFileName)
+        
         # Total address counts (residential, business, other)
         self.df['TOTAL_ADDRESS_COUNT'] = 0
         # Total vacant (residential, business, other)
@@ -28,14 +46,23 @@ class USPSData:
         self.df['TOTAL_NO_STAT'] = 0
         # Previous quarter vacant and no-stat currently in-service
         self.df['TOTAL_PQV_PQNS_IS'] = 0
-        self.df['QUARTER'] = quarter
+        self.df['QUARTER'] = self.quarter
+        self.df['PITT_TRACT'] = ''
         self.df = self.addColumns()
         self.df = self.writeToFile()
 
     def addColumns(self):
         def isNaN(string):
             return string != string
-
+        for index, row in self.df.iterrows():
+            tempgeoid = row.GEOID
+            tempgeoid = str(tempgeoid)
+            if tempgeoid.startswith('42'):
+                self.df.at[index, 'PITT_TRACT'] = True
+                #print("true")
+                
+        self.df = self.df.loc[(self.df['PITT_TRACT'] == True)]
+        
         for index, row in self.df.iterrows():
             temp_ams_res = row.AMS_RES
             temp_ams_bus = row.AMS_BUS
@@ -70,13 +97,36 @@ class USPSData:
         return self.df
 
     def writeToFile(self):
+        for index, row in self.df.iterrows():
+            tempquarter = row.QUARTER
+            tempquarter = str(tempquarter)
+            if '/' in tempquarter:
+                #print("Passing: "),
+                #print(tempquarter)
+                pass
+            else:
+                a = list(tempquarter)
+                #print(a)
+                #print(a[0])
+                if (len(a)) == 5:
+                    newQuarter = a[0] + '/' + a[1] + a[2] + a[3] + a[4]
+                    newQuarter = str(newQuarter)
+                    #print(newQuarter)
+                elif len(a) == 6:
+                    newQuarter = a[0] + a[1] + '/' + a[2] + a[3] + a[4] + a[5]
+                    newQuarter = str(newQuarter)
+                    #print(newQuarter)
+                self.df.at[index, 'QUARTER'] = newQuarter
+                
+        self.pghdf = self.pghdf.append(self.df)
         b = self.endFileName + "_Columns_Added.csv"
         self.df.to_csv(b)
-        print("output file: "),
+        c = self.finalPghFile
+        c = str(c)
+        c = "Added_Quarter_" + self.quarter+"_Total"+c
+        self.pghdf.to_csv(c)
+        print("quarter output file: "),
         print(b)
-
+        print("Total Pennsylvania Tracts file: "),
+        print(c)
         
-    #create an object of USPSData class with the file you want to add columns to
-    a=USPSData('USPS_09_2014.csv')
-    
-    #code writes output to a csv file and prints the name of the file when finished
